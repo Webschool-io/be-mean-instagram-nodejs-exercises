0e90872fede1bcf1a91c469d55242822be6b102c
Douglas Hennrich <douglas_b_h@hotmail.com>
2016-02-02 20:25:34 -0200

    Testing Jasmine integration

--- .TDD/index.js
+++ .TDD/index.js
@@ -1,5 +1,33 @@
-describe("A suite", function() {
+var fs = require('fs')
+  , path = require("path")
+  ;
+
+describe("Read File", function() {
+  var allReaded = true
+    , data = {}
+    ;
+
+  //
+  function readFiles(dirname, onFileContent, onError) {
+    console.log('dirname: ', dirname);
+
+    var files = fs.readdirSync(dirname);
+
+    files.forEach(function(filename) {
+      var content = fs.readFileSync(dirname + filename, 'utf-8');
+      onFileContent(filename, content);
+    });
+  }
+
   it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+
+    readFiles(path.join(__dirname, '../', 'class-02/'), function(filename, content) {
+        data[filename] = content;
+      }, function(error) {
+        throw err;
+    });
+
+    console.log('data: ', JSON.stringify(data));
+    expect(allReaded).toBe(true);
   });
 });
